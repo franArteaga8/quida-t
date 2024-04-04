45f5e81d803e65fe3ad27458b536b773649f06e6
Fran Arteaga <fran.am8@gmail.com>
2024-04-04 10:26:42 +0100

    RouterProvider + Theme Provider added in app.jsx Login Page restarted

--- root/src/pages/Login/Login.jsx
+++ root/src/pages/Login/Login.jsx
@@ -1,60 +1,7 @@
-import { Button, Card, CardActions, CardContent, CardHeader, Divider, TextField, Typography } from '@mui/material'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { login } from '../../services/auth'
 
 const Login = () => {
-
-  const [email, setEmail] = useState('')
-  const [pass, setPassword] = useState('')
-  const [errorMessage, setErrorMessage] = useState('')
-
-  const navigate = useNavigate()
-
-  const onLogin = async () => {
-    try {
-      const loginResponse = await login({email, pass})
-     //Do something with the response
-     localStorage.setItem('token', loginResponse.token)
-     navigate('/')
-     console.log(loginResponse)
-    } catch (error) {
-     //Handle the error
-    }
-  }
-  
   return (
-    <Card sx={{ maxWidth: '500px' }}>
-    <CardHeader title="Login" />
-    <CardContent>
-      <TextField
-        onChange={(e) => setEmail(e.target.value)}
-        label="Email"
-        variant="outlined"
-        fullWidth={true}
-        sx={{ marginBottom: '20px' }}
-      />
-      <TextField
-        onChange={(e) => setPassword(e.target.value)}
-        label="Password"
-        variant="outlined"
-        fullWidth={true}
-      />
-      {/* {errorMessage && (
-        <Typography color="error" textAlign="center" mt={2}>
-          {errorMessage}
-        </Typography>
-      )} */}
-    </CardContent>
-    <Divider />
-    <CardActions sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-      <Button /* onClick={() => goToRegister()} */>Register</Button>
-      <Button onClick={() => onLogin()} color="success">
-        Login
-      </Button>
-    </CardActions>
-  </Card>
-
+    <div>Login</div>
   )
 }
 
---
+++ root/src/theme/custom.jsx
@@ -0,0 +1,13 @@
+import {createTheme} from '@mui/material/styles'
+
+export const customTheme = createTheme({
+    palette: {
+        // palette
+    },
+    spacing: {
+        // spacing
+    },
+    typography: {
+        // typography
+    }
+})

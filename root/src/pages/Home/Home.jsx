3952309a0982a64598df3bd06d324d4a85510354
Aitor Díaz Santana <aitordd.ss@gmail.com>
2024-04-05 14:44:40 +0100

    start home

--- root/src/pages/Home/Home.jsx
+++ root/src/pages/Home/Home.jsx
@@ -1,7 +1,23 @@
+import './Home.css'
+import {Box, AppBar, Toolbar,Typography, Button} from '@mui/material';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div className="position">
+
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    </div>
+    </>
   )
 }
 

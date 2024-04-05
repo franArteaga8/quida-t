97363d8dfaf857a7df4a8f8b26671ea69989c580
Fran Arteaga <fran.am8@gmail.com>
2024-04-05 12:04:19 +0100

    Header: structured

--- root/src/components/Header/Header.jsx
+++ root/src/components/Header/Header.jsx
@@ -1,7 +1,36 @@
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material"
+import { Menu as Menuicon, } from '@mui/icons-material'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static" >
+            <Toolbar>
+              <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              >
+              <Menuicon/>
+              </IconButton>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                  quida-t
+                </Typography>
+              <IconButton>
+                <AccountCircleIcon sx={{ color: 'whitesmoke' }} />
+                
+              </IconButton>
+              
+            </Toolbar>
+
+          </AppBar>
+        </Box>
+    </>
   )
 }
 

96cff2bfff433b25fb7817781a7567bbd0f9a53a
Aitor Díaz Santana <aitordd.ss@gmail.com>
2024-04-08 12:15:00 +0100

    display general more date for send data

--- root/src/pages/Home/Home.jsx
+++ root/src/pages/Home/Home.jsx
@@ -1,20 +1,67 @@
 import './Home.css'
-import {Box, AppBar, Toolbar,Typography, Button} from '@mui/material';
+import {Box, AppBar, Toolbar, Typography} from '@mui/material';
+
+
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+
+
 
 const Home = () => {
+
+  const [date, setDate] = useState('')
+  const [selectDate, setSelectDate] = useState('')
+
+  const [taskDay, setTaskDay] = useState([])
+  const [taskWeek, setTaskWeek] = useState([])
+  const [taskMonth, setTaskMonth] = useState([])
+
+  const handleFormatDate = ()=>{
+    const dateCurrent = new Date()
+    const day = dateCurrent.getDate()
+    const month = dateCurrent.getMonth() + 1
+    const year =  dateCurrent.getFullYear()
+    setDate(`${month}/${day}/${year}`)
+  }
+
+
+
+  useEffect(()=>{
+    handleFormatDate()
+    
+  },[])
+
+
   return (
     <>
     <div className="position">
 
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+      <AppBar position="static" sx={{ flexGrow: 1 }}>
+        <Toolbar sx={{ flexGrow: 1 }} >
+
+            <label> Task</label>
+
+            <LocalizationProvider dateAdapter={AdapterDayjs} >
+              <DemoContainer components={['DatePicker']} >
+                <DatePicker value={dayjs(date)} onChange={(e)=> {setSelectDate(e)
+                }}/>
+              </DemoContainer>
+            </LocalizationProvider>
+
         </Toolbar>
       </AppBar>
+    </Box>
+
+    <Box>
+      <Typography variant='h1'>
+        
+        {selectDate && (parseInt(selectDate.$d.getMonth()) + 1)+ '/'+ selectDate.$d.getDate() + '/'+ selectDate.$d.getFullYear()}
+      </Typography>
     </Box>
     </div>
     </>

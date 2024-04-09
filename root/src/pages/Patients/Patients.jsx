4c35c37496fb5b24fb562aaa83efbe99ba4cfbd9
attetorres <atteneritorresgonzalez@gmail.com>
2024-04-09 14:20:19 +0100

    Patient Design Done

--- root/src/pages/Patients/Patients.jsx
+++ root/src/pages/Patients/Patients.jsx
@@ -1,12 +1,60 @@
-
-
+import React, { useState } from "react";
+import {
+  Avatar,
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 
 const Patients = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [patients, setPatients] = useState([]);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
 
-export default Patients
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
+
+  return (
+    <Box>
+      <Typography variant="h4" color={"primary.main"}>
+        Patients
+      </Typography>
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      <ListItemButton
+          selected={selectedIndex === 0}
+          onClick={(event) => handleListItemClick(event, 0)}
+        >
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="Patient 1"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline" }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  Lists
+                </Typography>
+                {" - Done"}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        </ListItemButton>
+        <Divider variant="inset" component="li" />
+      </List>
+    </Box>
+  );
+};
+
+export default Patients;

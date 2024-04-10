dfe754a3427ad7ab9f5a220686f47d2d4b57a4c9
Fran Arteaga <fran.am8@gmail.com>
2024-04-10 12:23:58 +0100

    List + Tasks styled

---
+++ root/src/components/TaskCard/TaskCard.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types'
+
+import { Edit, RemoveCircle } from '@mui/icons-material'
+import { Box,  Card, Divider, IconButton, Typography } from '@mui/material'
+
+const TaskCard = ({ task }) => {
+  return (
+    <Card sx={{ backgroundColor: 'peru', borderRadius:'10px', marginBottom: '20px', padding: '20px'}} >
+        <Box display={'flex'} justifyContent={'center'} alignItems={'center'}  >
+        <Typography variant='h6' >
+            {task.title}
+        </Typography>
+        <IconButton sx={{ marginLeft:'auto'}} >
+            <Edit color='primary' />
+            <RemoveCircle color='primary' />
+        </IconButton>
+        </Box>
+       <Divider/>
+        <Typography textAlign={'left'}>
+           description {task.description}
+        </Typography>
+    </Card>
+    
+  )
+}
+
+TaskCard.propTypes = {
+    task: PropTypes.object
+  }
+
+export default TaskCard

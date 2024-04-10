185bd1bf86b38ccb5a8c9bcc49b49404e966c6f5
Aitor Díaz Santana <aitordd.ss@gmail.com>
2024-04-10 14:10:55 +0100

    funtionality patient done less make pretty

--- root/src/components/PatientsList/PatientList.jsx
+++ root/src/components/PatientsList/PatientList.jsx
@@ -4,10 +4,12 @@ import {
   Accordion,
   AccordionSummary,
   AccordionDetails,
+  Divider
 } from "@mui/material";
+
 import { ArrowDownward } from "@mui/icons-material";
 import PropTypes from "prop-types";
-import { useEffect } from "react";
+
 
 const PatientList = ({patient}) => {
 
@@ -16,28 +18,31 @@ const PatientList = ({patient}) => {
   return (
     <>
     
-      
       <Accordion
           sx={{
             border: "1px green solid",
             borderColor: "primary.main",
             borderRadius: "10px",
             width: "100%"
-          }}
-        >
+          }}>
          
           <AccordionSummary
             expandIcon={<ArrowDownward sx={{ color: "secondary.main" }} />}
             aria-controls="panel1-content"
             id="panel1-header"
-            sx={{ backgroundColor: "primary.main" }}
-          >
+            sx={{ backgroundColor: "primary.main" }}>
+
             <Typography  textAlign={'left'} color={'secondary.main'} >{ patient.username}  </Typography>
-             </AccordionSummary>
+
+          </AccordionSummary>
+
           <AccordionDetails>
           
             <Typography  textAlign={'left'} color={'primary.main'} >Name: { patient.name}  </Typography>
             <Typography  textAlign={'left'} color={'primary.main'} >Last Name: { patient.lastName}  </Typography>
+
+            <Typography variant="h5" color={'primary.main'} > List Assigned</Typography>
+            <Divider></Divider>
 
           </AccordionDetails>
         </Accordion>
